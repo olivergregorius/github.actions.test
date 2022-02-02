b5174c6935ffdf56096fec5eadd6b69dee62f633
Oliver Gregorius <oliver@effitient.de>
2022-02-02 22:10:17 +0100

    Initial commit

---
+++ src/test/java/dev/gregorius/test/github/actions/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gregorius.test.github.actions.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
